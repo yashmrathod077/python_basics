0115c1d06887812517cfd823af6c98c0cb555d26
yashmrathod077 <yashmrathod077@gmail.com>
2026-03-15 21:42:03 +0530

    to find the maximum number

--- max.py
+++ max.py
@@ -1,1 +1,10 @@
-print("hellow world")
+num1 = int(input(" enter the first number"))
+num2 = int(input(" enter the second number "))
+num3 = int(input("enter the third number "))
+
+if num1>num2 and num1> num3:
+    print(f" { num1} is the maximum number ")
+elif num2>num3 and num2> num3 :
+    print(f" the {num2} is the maximum number ")
+else :
+    print(f" the { num3} is the maximum number ")
